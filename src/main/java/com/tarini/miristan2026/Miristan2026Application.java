7477a37821dee3142e29b8f6a14003da2127b00f
Prateek Kishore Karnad <trainer.it.ssk@gmail.com>
2025-12-26 16:46:04 +0530

    first commit

---
+++ src/main/java/com/tarini/miristan2026/Miristan2026Application.java
@@ -0,0 +1,13 @@
+package com.tarini.miristan2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Miristan2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Miristan2026Application.class, args);
+    }
+
+}
